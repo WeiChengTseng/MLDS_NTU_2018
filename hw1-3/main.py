f4d10bf8a3c4c6983b773be5fd742324aacefe2e
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-01-24 20:41:44 +0800

    hw1-2

--- hw1-3/main.py
+++ hw1-3/main.py
@@ -19,14 +19,13 @@ except:
     pass
 
 class DeepNet():
-    def __init__(self, in_dim=1, out_dim=1, model_type='regression', name='default', log_freq=2):
+    def __init__(self, in_dim=1, out_dim=1, hidden_neuron=0, model_type='regression', name='default'):
         self._name = name
-        self._log_freq = log_freq
         with tf.name_scope(name):
             self._x = tf.placeholder(dtype=tf.float32, shape=[None, in_dim])
             self._y = tf.placeholder(dtype=tf.float32, shape=[None, out_dim])
-            self._hidden_layer = tf.layers.dense(self._x, 20, activation=tf.nn.relu)
-            self._hidden_layer2 = tf.layers.dense(self._hidden_layer, 10, activation=tf.nn.relu)
+            self._hidden_layer = tf.layers.dense(self._x, 10+hidden_neuron, activation=tf.nn.relu)
+            self._hidden_layer2 = tf.layers.dense(self._hidden_layer, 10+hidden_neuron, activation=tf.nn.relu)
             self._out_layer = tf.layers.dense(self._hidden_layer2, out_dim)
             if model_type=='regression':
                 self._loss = tf.losses.mean_squared_error(self._out_layer, self._y)
@@ -34,6 +33,7 @@ class DeepNet():
                 self._loss = tf.losses.softmax_cross_entropy(self._y, self._out_layer)
             self._opt = tf.train.AdamOptimizer()
             self._train_step = self._opt.minimize(self._loss)
+            self._acc = tf.equal(tf.argmax(self._y, axis=1), tf.argmax(self._out_layer, axis=1))
         self._pca = sklearn.decomposition.PCA(n_components=2)
         self._list_parameters()
         self._dump_vars(init=True)
@@ -54,15 +54,15 @@ class DeepNet():
                 choice = np.random.choice(len(x), len(x), replace=False)
                 x, y = x[choice], y[choice]
 
-                if i % self._log_freq == 0:
+                if i % 50 == 0:
                     self._dump_vars(sess=sess, epoch=i, fd=fd)
 
             pred = sess.run(self._out_layer, feed_dict={self._x: x, self._y: y})
+
             self._visualize_opti_proc()
             self._visualize_norm()
             self._visualize_loss(his)
-            self._visualize_eigen_value(his)
-        return his, pred
+        return his, pred, self._count_para()
 
     def _list_parameters(self):
         for var in tf.trainable_variables():
@@ -75,30 +75,20 @@ class DeepNet():
             w_reshape = [tf.reshape(w, [-1]) for w in weight]
             self._w_concat = tf.concat(w_reshape, 0)
             self._norm = tf.add_n([tf.norm(tf.gradients(self._loss, w)) for w in weight])
-            # print(w_reshape)
-            self._hessians = tf.hessians([self._loss], weight)
-            # print(self._hessians)
-            prod = lambda a: [a[0]*a[1], a[2]*a[3]]
-            self._eigen_val = tf.concat([tf.linalg.eigvalsh(tf.reshape(var, prod(var.get_shape()))) for var in self._hessians], 0)
-            # print(self._eigen_val)
         else:
-            pickle.dump(self._w_concat.eval(), open('hw1-2/logs/weights_%d.p' % epoch, 'wb'))
-            pickle.dump(self._norm.eval(feed_dict=fd), open('hw1-2/logs/norm_%d.p' % epoch, 'wb'))
-            eigen_val = self._eigen_val.eval(feed_dict=fd)
-            pickle.dump(eigen_val, open('hw1-2/logs/eigen_val_%d.p' % epoch, 'wb'))
+            pickle.dump(self._w_concat.eval(), open('hw1-2/weights_%d.p' % epoch, 'wb'))
+            pickle.dump(self._norm.eval(feed_dict=fd), open('hw1-2/norm_%d.p' % epoch, 'wb'))
         return
 
     def _count_para(self):
         return sum([np.prod(list(var.get_shape())) for var in tf.trainable_variables()])
 
     def _visualize_opti_proc(self):
-        num = lambda x: int(x[19: -2])
-        weights = np.array([pickle.load(open(f, 'rb')) 
-                            for f in sorted(glob.glob('hw1-2/logs/weights*.p'), key=num)])
+        weights = np.array([pickle.load(open(f, 'rb')) for f in sorted(glob.glob('hw1-2/weights*.p'))])
         w_pca = self._pca.fit_transform(weights)
-        plt.plot(w_pca[:, 0], w_pca[:, 1], '-o', alpha=0.7)
+        plt.scatter(w_pca[:, 0], w_pca[:, 1])
         for i in range(len(w_pca)):
-            plt.text(w_pca[i, 0]+0.1, w_pca[i, 1]+0.2, str(self._log_freq*i))
+            plt.text(w_pca[i, 0], w_pca[i, 1]+0.2, str(50*i))
         plt.title('Optimization Process')
         plt.xlabel('PC 0')
         plt.ylabel('PC 1')
@@ -107,9 +97,7 @@ class DeepNet():
         return
     
     def _visualize_norm(self):
-        num = lambda x: int(x[16: -2])
-        norms = np.array([pickle.load(open(f, 'rb')) 
-                          for f in sorted(glob.glob('hw1-2/logs/norm*.p'), key=num)])
+        norms = np.array([pickle.load(open(f, 'rb')) for f in glob.glob('hw1-2/norm*.p')])
         plt.plot(range(len(norms)), norms)
         plt.title('Gradient Norm During Training')
         plt.yscale('log')
@@ -128,32 +116,13 @@ class DeepNet():
         plt.close()
         return
 
-    def _visualize_eigen_value(self, loss):
-        loss = np.array(loss)[np.arange(len(loss)//self._log_freq)*self._log_freq]
-        num = lambda x: int(x[21: -2])
-        eigen_val = np.array([pickle.load(open(f, 'rb')) 
-                            for f in sorted(glob.glob('hw1-2/logs/eigen_val_*.p'), key=num)])
-        print(sorted(glob.glob('hw1-2/logs/eigen_val_*.p'), key=num))
-        eigen_val[eigen_val<=0] = 0
-        non_zero = np.count_nonzero(eigen_val, axis=1)
-        minimal_ratio = non_zero / eigen_val.shape[1]
-        plt.scatter(minimal_ratio, loss, alpha=0.7, marker='.')
-        plt.xlabel('minimal ratio')
-        plt.ylabel('loss')
-        plt.savefig('hw1-2/minimal_ratio.png', dpi=500)
-        return
-
 if __name__ == '__main__':
     plt.rcParams["figure.figsize"] = (12,6.75)
-    # (x_mnist, y_), (_, __) = tf.keras.datasets.mnist.load_data()
-    # x_mnist, y_mnist = x_mnist.reshape([x_mnist.shape[0], -1]), np.zeros((len(x_mnist), 10))
-    # y_mnist[np.arange(60000), y_] = 1
-
-    x_sinc = np.linspace(-1, 1, 400)
-    y_sinc = np.sinc(x_sinc * 5)
-    x_sinc, y_sinc = x_sinc[:, np.newaxis], y_sinc[:, np.newaxis]
-
-    # net2 = DeepNet(in_dim=28*28, out_dim=10, model_type='classification')
-    # his2, _ = net2.train(x_mnist, y_mnist, epoch=20)
-    net2 = DeepNet(in_dim=1, out_dim=1)
-    his2, _ = net2.train(x_sinc, y_sinc, epoch=100)
+    (x_mnist, y_), (_, __) = tf.keras.datasets.mnist.load_data()
+    x_mnist, y_mnist = x_mnist.reshape([x_mnist.shape[0], -1]), np.zeros((len(x_mnist), 10))
+    y_mnist[np.arange(60000), y_] = 1
+    para = []
+    for i in np.arange(100):
+        net2 = DeepNet(in_dim=28*28, out_dim=10, hidden_neuron=i, model_type='classification')
+        his2, _, num_para = net2.train(x_mnist, y_mnist, epoch=2000)
+        para.append(num_para)
